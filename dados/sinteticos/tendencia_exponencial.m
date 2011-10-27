dd04112ec6033fe683b30d60f6280e7e71929964
Fernando Guimarães Ferreira <fernandogferreira@gmail.com>
2011-10-27 11:15:15 -0200

    dados not on git

--- dados/sinteticos/tendencia_exponencial.m
+++
@@ -1,8 +0,0 @@
-function [xte]=tendencia_exponencial(coef,amostras,snr);
-%coef: coeficiente coef*i/amostras
-%amostras
-%relacao sinal ruido
-
-for i=1:amostras
-    xte(i)=exp(coef*(i/(amostras)))+(randn(1)*snr);
-end
